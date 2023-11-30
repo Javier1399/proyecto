59fcc5fc137338d5f29772675c47b05e1c528159
Javier1399 <150076480+Javier1399@users.noreply.github.com>
2023-11-29 23:56:45 -0600

    Delete TrabajadorBase.cpp

--- TrabajadorBase.cpp
+++
@@ -1,8 +0,0 @@
-#include "TrabajadorBase.h"
-
- TrabajadorBase::TrabajadorBase(std::string nombre, int id, int horas)
-     : Trabajador(nombre, id, horas) {}
-
- double TrabajadorBase::calcularSueldo() const {
-     return sueldo_base;
- }
