ecd3112e16989501ce5412a4cd588dbc42e16b1a
Javier1399 <150076480+Javier1399@users.noreply.github.com>
2023-11-29 23:58:27 -0600

    Delete Gerente.cpp

--- Gerente.cpp
+++
@@ -1,8 +0,0 @@
-#include "Gerente.h"
-
-Gerente::Gerente(std::string nombre, int id, int horas, double sueldo)
-    : Trabajador(nombre, id, horas), sueldo(sueldo) {}
-
-double Gerente::calcularSueldo() const {
-    return sueldo;
-}
